d072bff539026aafc6d46c595064fcb68040e2f8
mgvaldes <mgvaldesgraterol@gmail.com>
2016-10-27 11:21:15 +0200

    eliminando archivos de plink

--- feature_info.R
+++
@@ -1,3 +0,0 @@
-library(data.table)
-
-features.info <- fread("/home/gvaldes/Documentos/master-thesis/health-forecast-project/health-forecast/fs/raw/filter/anova/classifiers/linear_svm/genomic/features_info.csv", data.table = FALSE, header = TRUE)
