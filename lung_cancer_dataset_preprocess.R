c5ed83fdd93ba4c72d2ff18e9a696c6da9506c0f
mgvaldes <mgvaldesgraterol@gmail.com>
2017-01-09 12:55:43 +0100

    adding lung_cancer_dataset_preprocess.R

---
+++ lung_cancer_dataset_preprocess.R
@@ -0,0 +1,21 @@
+library(data.table)
+
+raw.file <- fread("chr1_imputed_maf001.raw", data.table = FALSE, header = TRUE)
+raw.file <- raw.file[, 7:ncol(raw.file)]
+
+pheno.data <- fread("IMPPC_lung_progres.txt", header = TRUE, data.table = FALSE)
+pheno.data <- pheno.data[, 11:17]
+pheno.data <- sapply(pheno.data, as.numeric)
+pheno.data[, "progres"] <- pheno.data[, "progres"] - rep(1, nrow(pheno.data))
+
+library(mice)
+
+imp <- mice(pheno.data, m = 1)
+pheno.data <- complete(imp)
+
+################ complete raw data
+raw.data.with.pheno.data <- cbind(pheno.data[, 1:6], raw.file)
+raw.data.with.pheno.data <- cbind(pheno.data[, 7], raw.data.with.pheno.data)
+colnames(raw.data.with.pheno.data)[1] <- "progres"
+
+write.table(raw.data.with.pheno.data, file = "complete_dataset_with_pheno.csv", row.names = FALSE, col.names = TRUE, sep = ",", quote = FALSE)
